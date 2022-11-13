08479872d3fd74728437488d09bd345f59e8dee4
Saurabh Khatmode <khatmodesaurabh7@gmail.com>
2022-11-13 17:54:46 +0530

    added new project

--- src/helpers/ProjectList.jsx
+++ src/helpers/ProjectList.jsx
@@ -2,41 +2,51 @@ import PortfolioLogo from '../assets/Protfolio-logo.jpg'
 import RecipeFinder from '../assets/Recipe-Finder-Logo.jpg'
 import WeatherApp from '../assets/Weather-App-Logo.jpg'
 import TextEditor from '../assets/Text-Editor-logo.jpg'
+import IMDBLogo from '../assets/IMDB logo.jpg'
 export const ProjectList = [
     {
         key: 1,
-        name: "Personal Portfolio",
-        image: PortfolioLogo,
-        skills: "React JS , CSS",
-        codeLink: "https://github.com/Saurabh-8585/Saurabh-Dev",
-        demoLink:"https://saurabhkhatmode.netlify.app/",
-        info: "This is a Personal Portfolio to showcase my projects,skills and you will find information about me"
+        name: "IMDB-Movie app Clone",
+        image: IMDBLogo,
+        skills: "React JS ,HTML, CSS",
+        codeLink: "https://github.com/Saurabh-8585/IMDB-Clone",
+        demoLink: "https://saurabh-imdbclone.netlify.app/",
+        info: "This is IMDB clone website where you can search any movie according to you. main features trailer,cast,reviews"
     },
     {
         key: 2,
+        name: "Personal Portfolio",
+        image: PortfolioLogo,
+        skills: "React JS ,HTML, CSS",
+        codeLink: "https://github.com/Saurabh-8585/Saurabh-Dev",
+        demoLink: "https://saurabhkhatmode.netlify.app/",
+        info: "This is a Personal Portfolio to showcase my projects,skills and you will find information about me"
+    },
+    {
+        key: 3,
         name: "Recipe Finder",
         image: RecipeFinder,
-        skills: "React JS , Material UI , CSS",
-        codeLink:"https://github.com/Saurabh-8585/Recipe-Finder",
+        skills: "React JS , Material UI ,HTML, CSS",
+        codeLink: "https://github.com/Saurabh-8585/Recipe-Finder",
         demoLink: "https://saurabh-8585.github.io/Recipe-Finder/",
         info: "This is recipe finder web application based on Edamam API .This website will display ingredients, all details (cuisine,calories, etc) "
     },
     {
-        key: 3,
+        key: 4,
         name: "Real Time Weather App",
         image: WeatherApp,
-        skills: "React JS , CSS",
-        codeLink:"https://github.com/Saurabh-8585/WeatherApp",
-        demoLink:"https://saurabh-8585.github.io/WeatherApp/",
+        skills: "React JS ,HTML, CSS",
+        codeLink: "https://github.com/Saurabh-8585/WeatherApp",
+        demoLink: "https://saurabh-8585.github.io/WeatherApp/",
         info: "This is real time Weather Web Application based on OpenWeatherAPI. It displays The current temperature as well as ,max-min temperature , weather,wind speed,sunrise,sunset,humidity,pressure "
     },
     {
-        key: 4,
+        key: 5,
         name: "Text Editor",
         image: TextEditor,
         skills: "React JS ,Bootstrap",
         codeLink: "https://github.com/Saurabh-8585/TextUtils",
-        demoLink:"https://saurabh-8585.github.io/TextUtils/",
+        demoLink: "https://saurabh-8585.github.io/TextUtils/",
         info: "This is my First React JS project . It is simple text editor you can perform editing on text in that web application and you preview it"
     },
 ]
